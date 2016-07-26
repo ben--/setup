9d310aac9bfccc6aafe539994ad4f7cd59dc991e
Ben Rogers <benrogers@mac.com>
2016-07-26 08:49:41 -0400

    First pass at an xmonad installer

---
+++ xmonad/skel/.xmonad/xmonad.hs
@@ -0,0 +1,20 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    spawn "xrandr --output default --mode 2560x1440"
+    xmproc <- spawnPipe "/usr/bin/xmobar /home/pair/.xmobarrc"
+    xmonad $ defaultConfig
+        { manageHook = manageDocks <+> manageHook defaultConfig
+        , layoutHook = avoidStruts  $  layoutHook defaultConfig
+        , logHook = dynamicLogWithPP xmobarPP
+                        { ppOutput = hPutStrLn xmproc
+                        , ppTitle = xmobarColor "green" "" . shorten 50
+                        }
+        , modMask = mod3Mask
+        --, modMask = mod4Mask     -- Rebind Mod to the Windows key
+        }
